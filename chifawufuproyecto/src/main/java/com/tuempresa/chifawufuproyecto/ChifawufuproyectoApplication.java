c43030bf7046bb3550fbd733e18c663fd8363339
Jeanp15 <jhonatan15021995@gmail.com>
2025-11-12 22:32:12 -0500

    Add files via upload

---
+++ chifawufuproyecto/src/main/java/com/tuempresa/chifawufuproyecto/ChifawufuproyectoApplication.java
@@ -0,0 +1,13 @@
+package com.tuempresa.chifawufuproyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChifawufuproyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChifawufuproyectoApplication.class, args);
+	}
+
+}
